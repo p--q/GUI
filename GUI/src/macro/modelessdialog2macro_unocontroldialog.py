82b45e63f3ad1618a41049f0bbf7d5be041c4f89
pq <pq.blogspot@gmail.com>
2017-08-22 00:56:56 +0900

    HM

--- GUI/src/macro/modelessdialog2macro_unocontroldialog.py
+++ GUI/src/macro/modelessdialog2macro_unocontroldialog.py
@@ -53,6 +53,7 @@ def frameCreator(ctx, smgr, parentframe): # 新しいフレームを追加する
 def dialogCreator(ctx, smgr, dialogprops):  # ダイアログと、それにコントロールを追加する関数を返す。まずダイアログモデルのプロパティを取得。
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     dialog = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialog", ctx)  # ダイアログの生成。
     dialog.setPosSize(dialogprops.pop("PositionX"), dialogprops.pop("PositionY"), dialogprops.pop("Width"), dialogprops.pop("Height"), POSSIZE)  # ダイアログモデルのプロパティで設定すると単位がMapAppになってしまうのでコントロールに設定。
     dialogmodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialogModel", ctx)  # ダイアログモデルの生成。
@@ -88,6 +89,43 @@ def dialogCreator(ctx, smgr, dialogprops):  # ダイアログと、それにコ�
             i += 1
         return name  
     return dialog, addControl  # ダイアログとそのダイアログにコントロールを追加する関数を返す。
+=======
+	dialog = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialog", ctx)  # ダイアログの生成。
+	dialog.setPosSize(dialogprops.pop("PositionX"), dialogprops.pop("PositionY"), dialogprops.pop("Width"), dialogprops.pop("Height"), POSSIZE)  # ダイアログモデルのプロパティで設定すると単位がMapAppになってしまうのでコントロールに設定。
+	dialogmodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialogModel", ctx)  # ダイアログモデルの生成。
+	dialogmodel.setPropertyValues(tuple(dialogprops.keys()), tuple(dialogprops.values()))  # ダイアログモデルのプロパティを設定。
+	dialog.setModel(dialogmodel)  # ダイアログにダイアログモデルを設定。
+	dialog.setVisible(False)  # 描画中のものを表示しない。
+	def addControl(controltype, props, attrs=None):  # props: コントロールモデルのプロパティ、attr: コントロールの属性。
+		control = smgr.createInstanceWithContext("com.sun.star.awt.UnoControl{}".format(controltype), ctx)  # コントロールを生成。
+		control.setPosSize(props.pop("PositionX"), props.pop("PositionY"), props.pop("Width"), props.pop("Height"), POSSIZE)  # ピクセルで指定するために位置座標と大きさだけコントロールで設定。
+		if not "Name" in props:
+			props["Name"] = _generateSequentialName(controltype)  # Nameがpropsになければ通し番号名を生成。
+		controlmodel = dialogmodel.createInstance("com.sun.star.awt.UnoControl{}Model".format(controltype))  # コントロールモデルを生成。UnoControlDialogElementサービスのためにUnoControlDialogModelからの作成が必要。
+		values = props.values()  # プロパティの値がタプルの時にsetProperties()でエラーが出るのでその対応が必要。
+		if any(map(isinstance, values, [tuple]*len(values))):
+			[setattr(controlmodel, key, val) for key, val in props.items()]  # valはリストでもタプルでも対応可能。XMultiPropertySetのsetPropertyValues()では[]anyと判断されてタプルも使えない。
+		else:
+			controlmodel.setPropertyValues(tuple(props.keys()), tuple(values))
+		control.setModel(controlmodel)  # コントロールにコントロールモデルを設定。
+		dialog.addControl(props["Name"], control)  # コントロールをダイアログに追加。
+		if attrs is not None:  # Dialogに追加したあとでないと各コントロールへの属性は追加できない。
+			control = dialog.getControl(props["Name"])  # ダイアログに追加された後のコントロールを取得。
+			for key, val in attrs.items():  # メソッドの引数がないときはvalをNoneにしている。
+				if val is None:
+					getattr(control, key)()
+				else:
+					getattr(control, key)(val)
+	def _generateSequentialName(controltype):  # 連番名の作成。
+		i = 1
+		flg = True
+		while flg:
+			name = "{}{}".format(controltype, i)
+			flg = dialog.getControl(name)  # 同名のコントロールの有無を判断。
+			i += 1
+		return name  
+	return dialog, addControl  # ダイアログとそのダイアログにコントロールを追加する関数を返す。
+>>>>>>> refs/heads/feature/201707171853model
 =======
 	dialog = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialog", ctx)  # ダイアログの生成。
 	dialog.setPosSize(dialogprops.pop("PositionX"), dialogprops.pop("PositionY"), dialogprops.pop("Width"), dialogprops.pop("Height"), POSSIZE)  # ダイアログモデルのプロパティで設定すると単位がMapAppになってしまうのでコントロールに設定。
